41fbb372836836fd25ce9bbece73fca1d9cc1cf3
Hongyi Zheng <zhenghongyi2000@hotmail.com>
2023-04-06 14:20:32 -0400

    add ul2 support

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 from datasets import concatenate_datasets
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+import torch
 import time
 import random
 from openai.error import APIError, APIConnectionError, RateLimitError, Timeout
@@ -34,6 +36,24 @@ def generate_exemplar(exemp_dict, prompt, perturb):
         pass
 
     return exemplar
+
+
+def generate_prompt(question, exemplar, prompt):
+
+    instr = "End your response with 'The answer is <answer>.'"
+
+    if prompt == 'cot':
+        prompt_text = instr + "\n\n" + exemplar + \
+            "\n\nQ: " + question + "\nA:"
+
+    elif prompt == '0cot':
+        prompt_text = instr + "\n\nQ: " + \
+            question + "\nA: Let's think step by step: "
+
+    else:
+        pass
+
+    return prompt_text
 
 
 def perturb_question(sample, perturb):
@@ -100,12 +120,31 @@ def build_record(sample, result, perturb):
         except AttributeError:
             record['numeric_response'] = None
 
+    elif result['model'] == 'ul2':
+        record['response'] = result['response']
+        try:
+            record['numeric_response'] = re.search(
+                r'The answer is (.*?)\.', result['response'], re.IGNORECASE).group(1)
+        except AttributeError:
+            record['numeric_response'] = None
+
     return record
 
 
-def evaluate_openai(run_id, model, dataset, prompt, perturb, perturb_exemplar):
+def fetch_model_and_tokenizer(model_name):
+    if model_name == 'ul2':
+        model_file = T5ForConditionalGeneration.from_pretrained(
+            "google/flan-ul2", device_map="auto", load_in_8bit=True)
+        model_tokenizer = AutoTokenizer.from_pretrained("google/flan-ul2")
+
+        return model_file, model_tokenizer
+    else:
+        return None, None
+
+
+def evaluate_openai(run_id, model_name, dataset, prompt, perturb, perturb_exemplar):
     with open(f'logs/{run_id}.jsonl', 'w') as f:
-        instr = "End your response with 'The answer is <answer>.'"
+
         exemp_dict = dataset["train"][0]
 
         # generate exemplar
@@ -114,50 +153,32 @@ def evaluate_openai(run_id, model, dataset, prompt, perturb, perturb_exemplar):
 
         # merge train and test datasets and remove sample for exemplar
         modified_ds = concatenate_datasets([dataset["train"].select(
-            range(1690, len(dataset["train"]))), dataset["test"]])
+            range(1, 5)), dataset["test"]])
 
-        engine = 'text-davinci-003' if model == 'gpt3' else 'gpt-3.5-turbo'
+        model_file, model_tokenizer = fetch_model_and_tokenizer(model_name)
 
-        if prompt == 'cot':
+        for sample in tqdm(modified_ds):
 
-            for sample in tqdm(modified_ds):
+            # generate question text
+            question = perturb_question(sample, perturb)
+            # generate prompt text
+            prompt_text = generate_prompt(question, exemplar, prompt)
+            # get response
+            result = generate_response(
+                prompt_text, model_name, model_file, model_tokenizer)
 
-                question = perturb_question(sample, perturb)
+            record = build_record(sample, result, perturb)
+            f.write(json.dumps(record) + '\n')
 
-                prompt = instr + "\n\n" + exemplar + \
-                    "\n\nQ: " + question + "\nA:"
-
-                result = generate_response(prompt, engine).to_dict()
-
-                record = build_record(sample, result, perturb)
-                f.write(json.dumps(record) + '\n')
-
-        elif prompt == '0cot':
-
-            for sample in tqdm(modified_ds):
-
-                question = perturb_question(sample, perturb)
-
-                prompt = instr + "\n\nQ: " + \
-                    question + "\nA: Let's think step by step: "
-
-                result = generate_response(prompt, engine).to_dict()
-
-                record = build_record(sample, result, perturb)
-                f.write(json.dumps(record) + '\n')
-
-        else:
-            pass
-
-# Define a function to generate responses using GPT-3
+# Function to interact with the model and generate a response
 
 
-def generate_response(prompt, engine):
-    if engine == 'text-davinci-003':
+def generate_response(prompt, model_name, model_file, model_tokenizer):
+    if model_name == 'gpt3':
         while True:
             try:
                 response = openai.Completion.create(
-                    engine=engine,
+                    engine='text-davinci-003',
                     prompt=prompt,
                     max_tokens=300,
                     temperature=0,
@@ -165,25 +186,38 @@ def generate_response(prompt, engine):
                     frequency_penalty=0.0,
                     presence_penalty=0.0,
                     logprobs=1,
-                )
+                ).to_dict()
             except (APIError, OSError, APIConnectionError, RateLimitError, Timeout):
                 time.sleep(1)
                 continue
             break
-    else:
+    elif model_name == 'gptturbo':
         while True:
             try:
                 response = openai.ChatCompletion.create(
-                    model=engine,
+                    model='gpt-3.5-turbo',
                     messages=[{"role": "user", "content": prompt}],
                     max_tokens=300,
                     temperature=0,
                     top_p=1,
                     frequency_penalty=0.0,
                     presence_penalty=0.0,
-                )
+                ).to_dict()
             except (APIError, OSError, APIConnectionError, RateLimitError, Timeout):
                 time.sleep(1)
                 continue
             break
+    elif model_name == 'ul2':
+        inputs = model_tokenizer(
+            prompt, return_tensors="pt").input_ids.to("cuda")
+        outputs = model_file.generate(inputs, max_length=300, do_sample=True,
+                                      top_p=1, temperature=0, num_return_sequences=1)
+        answer_text = model_tokenizer.decode(
+            outputs[0], skip_special_tokens=True)
+
+        response = {
+            'model': 'ul2',
+            'response': answer_text,
+        }
+
     return response
